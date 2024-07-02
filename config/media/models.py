de604c81b1fdb312225f77c4811a6e4fdda860ac
anna woodward <annagracewoodward@gmail.com>
2024-07-02 09:49:54 -0500

    Create Author and Book Django models

--- config/media/models.py
+++ config/media/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    authors = models.ManyToManyField(Author, related_name='books')
+    kindle = models.BooleanField(null=True)
+    physical = models.BooleanField(null=True)
+    date_finished = models.DateField()
+    times_read = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
+
+    
